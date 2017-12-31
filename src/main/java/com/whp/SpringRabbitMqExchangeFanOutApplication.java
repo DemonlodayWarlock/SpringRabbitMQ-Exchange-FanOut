df71a1771fb535d1a748a5fb8ed657a57e2afcef
mac <mac@192.168.1.9>
2017-12-31 22:31:25 +0800

    Spring RabbitMQ Exchange type for FanOut.
    本例子是关于spring rabbitmq Fanout Exchange 消息广播的模式的实现。
    
    创建一个新的exchange "fanoutExchange",
    创建三个queue ：
    Queue fanout.A
    Queue fanout.B
    Queue fanout.C
    
    当一个message被发出，会同时推送到三个queue,message不会在queue中停留。

---
+++ src/main/java/com/whp/SpringRabbitMqExchangeFanOutApplication.java
@@ -0,0 +1,12 @@
+package com.whp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitMqExchangeFanOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitMqExchangeFanOutApplication.class, args);
+	}
+}
